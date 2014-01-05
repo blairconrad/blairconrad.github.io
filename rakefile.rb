31142b8038ea69386feb6a4538cd6c3a9e6ef8e7
Blair Conrad <blair.conrad@gmail.com>
2014-01-05 11:52:57 -0500

    when running locally, we set the site url to be localhost:4000

--- rakefile.rb
+++ rakefile.rb
@@ -5,5 +5,5 @@ task :serve do
     puts '* Changing the codepage'
     `chcp 65001`
     puts '* Running Jekyll'
-    `jekyll serve --watch`
+    `jekyll serve --watch --config _config.yml,_config.local.yml`
 end
