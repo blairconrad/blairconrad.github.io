7089681cda85ace6ebfa4b12736faea3fab92c23
Blair Conrad <blair@blairconrad.com>
2016-11-13 09:26:59 -0500

    Prevent duplicate recipes

--- Recipes/_new.py
+++ Recipes/_new.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
+import os.path
 import sys
-import subprocess
+
 
 def main(args=sys.argv[1:]):
     title = ' '.join(args)
     t = file('_template.md', 'rb').read() % vars()
-    filename = ''.join([a.capitalize() for a in args]).replace("'", "").replace(" ", "")
-    filename = filename[0].lower() + filename[1:] + '.md'
+    basename = (''.join([a.capitalize() for a in args])
+                .translate(None, "' "))
+    basename = basename[0].lower() + basename[1:]
+    filename = basename + '.md'
+
+    if os.path.exists(filename) or os.path.exists(basename + '.html'):
+        raise Exception('we already have a recipe for ' + title)
 
     with file(filename, 'wb') as f:
         f.write(t)
@@ -17,4 +23,3 @@ def main(args=sys.argv[1:]):
 
 if __name__ == '__main__':
     sys.exit(main())
-
