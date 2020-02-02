3e5e17d5e19516a6f0af84c1c61a5c24f4f849e4
Blair Conrad <blair@blairconrad.com>
2020-02-02 18:41:34 -0500

    Remove indent from paragraphs

--- src/pelicanconf.py
+++ src/pelicanconf.py
@@ -68,6 +68,7 @@ M_HIDE_ARTICLE_SUMMARY = True
 M_CSS_FILES = [
     "https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i%7CSource+Code+Pro:400,400i,600",
     "/static/m-dark.css",
+    "/static/custom.css",
 ]
 M_THEME_COLOR = "#22272e"
 
