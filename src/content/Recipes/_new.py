b45565d7f2006ae66bc05b6a9b8f75acac9b4d7e
Blair Conrad <blair@blairconrad.com>
2021-03-29 21:29:09 -0400

    Make _new recipe script work again

--- src/content/Recipes/_new.py
+++ src/content/Recipes/_new.py
@@ -20,27 +20,6 @@ def main(args=sys.argv[1:]):
         with open(filename, "w") as new_recipe_file:
             new_recipe_file.write(content)
 
-    with open("index.md", "r") as index_in:
-        index_lines = [l.strip() for l in index_in.readlines()]
-
-    done = False
-    index = 0
-    for index_line in index_lines:
-        if index_line.startswith("* ["):
-            if index_line[3:] > title:
-                index_lines.insert(index, "* [" + title + "](" + basename + ")")
-                done = True
-                break
-        index += 1
-
-    if not done:
-        index_lines.append("* [" + title + "](" + basename + ")")
-
-    with open("index.md", "w") as index_out:
-        for line in index_lines:
-            index_out.write(line)
-            index_out.write("\n")
-
     return 0
 
 
